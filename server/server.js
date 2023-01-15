e9a757c5872d3185e22d00dd8368ac65de1fa189
s-techlead <sebastienschnd.techlead@gmail.com>
2023-01-15 11:38:35 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import express from 'express';
+import devBundle from './devBundle';
+import template from './../template';
+import { MongoClient } from 'mongodb';
+
+const app = express();
+devBundle.compile(app)
+
+const CURRENT_WORKING_DIR = process.cwd()
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+app.get('/', (req, res) => {
+    res.status(200).send(template())
+})
+
+let port = process.env.PORT || 3000;
+app.listen(port, function onStart(err) {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Server started on port %s', port)
+})
+
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
+MongoClient.connect(url, (err, db) => {
+    console.log("Connected successfully to mongodb server")
+    db.close()
+})
